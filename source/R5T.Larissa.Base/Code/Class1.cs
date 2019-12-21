cbdfffd715466ae5b1cbfcc9577b33cb36ecbfae
MinexAutomation <minex.automation@gmail.com>
2019-12-21 00:46:13 -0800

    Defined the ISvnOperator service definition interface type.

--- source/R5T.Larissa.Base/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Larissa.Base
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Larissa.Base/Code/Services/Definitions/ISvnOperator.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+
+namespace R5T.Larissa
+{
+    public interface ISvnOperator
+    {
+    }
+}
