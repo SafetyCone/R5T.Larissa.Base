5a5494f7df6c55b896a5f72cc754b01dfe3f416e
MinexAutomation <minex.automation@gmail.com>
2019-12-22 23:45:30 -0800

    Added GetRemoteRepositoryUrl() and Update() methods.

--- source/R5T.Larissa.Base/Code/Services/Definitions/ISvnOperator.cs
+++ source/R5T.Larissa.Base/Code/Services/Definitions/ISvnOperator.cs
@@ -8,6 +8,8 @@ namespace R5T.Larissa
         void Add(string path);
         void Checkout(string repositoryUrl, string localDirectoryPath);
         void Commit(string path, string message, bool directoryOnly = false);
+        string GetRemoteRepositoryUrl(string path);
         Version GetVersion();
+        void Update(string path);
     }
 }
