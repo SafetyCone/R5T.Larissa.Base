2a35cebbaccad6af14e03068f28cd38e20e7738d
MinexAutomation <minex.automation@gmail.com>
2020-06-08 19:56:22 -0700

    Added username and password for commands.

--- source/R5T.Larissa.Base/Code/Services/Definitions/ISvnOperator.cs
+++ source/R5T.Larissa.Base/Code/Services/Definitions/ISvnOperator.cs
@@ -6,7 +6,7 @@ namespace R5T.Larissa
     public interface ISvnOperator
     {
         void Add(string path);
-        void Checkout(string repositoryUrl, string localDirectoryPath);
+        void Checkout(string repositoryUrl, string localDirectoryPath, string username, string password);
         void Commit(string path, string message, bool directoryOnly = false);
         string GetRemoteRepositoryUrl(string path);
         Version GetVersion();
